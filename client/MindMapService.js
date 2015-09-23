3c18773df9ee20982418522e25ad6ad13ea0a739
Rupali Gangarde <rupalig@thoughtworks.com>
2015-09-23 19:31:14 +0530

    create blank canvas

--- client/MindMapService.js
+++ client/MindMapService.js
@@ -4,21 +4,7 @@
 
 MindMapService = function MindMapService() {};
 
-MindMapService.prototype.sayHello = function(name) {
-  return 'Hello ' + name;
+MindMapService.prototype.createNode = function(name) {
+    return Mindmaps.insert({name:name,children:[]});
 };
-SessionCounter = {
-  getCount :function(){
-    
-    var count =  Session.get('counter');
-    if(!count){
-      Session.setDefault('counter', 0);
-      return 0;
-    }
-    return count;
-  },
-  
-  incrementCount:function(){
-    Session.set('counter', SessionCounter.getCount('counter') + 1);
-  }
-}
+
--- mindmap.js
+++ mindmap.js
@@ -1,26 +1,49 @@
 /* global Template */
 /* global SessionCounter */
 /* global Meteor */
+
+Mindmaps= new Meteor.Collection("Mindmaps");
+var mindMapService;
+Router
+.configure({
+	layoutTemplate: 'main'
+});
+Router.route('/', { template: 'home' });
+Router.route('/create/:_id',function () {
+	this.render('create',{
+		data: function () {
+			return Mindmaps.findOne(this.params._id);
+		}
+	});
+});
+
 if (Meteor.isClient) {
   // counter starts at 0
+Template.create.rendered = function(){
+	var width=800,height=800,
+		tree = d3.layout.tree().size([ 700,700]),
+		svg = d3.select('#mindmap').attr('width', width).attr('height',height),
+		rootNode = {_id:1,name:'New Template'};
+	var treeNodes = tree.nodes(rootNode).reverse(),
+		links = tree.links(treeNodes);
+		
 
-  Template.hello.helpers({
-    counter: function () {
-      return SessionCounter.getCount();
-    }
-  });
+}
+mindMapService   = new MindMapService();
+Template.MyButton.events({
+	'click #clickme': function () {
+  	// 1. cretate root node with defualt title
+  	var mindMapId = mindMapService.createNode('New Mindmap'),
+  	link = '/create/'+mindMapId;
+  	// 2. Go to canvas root note
+  	Router.go(link);	
+  }
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      SessionCounter.incrementCount();
-
-    }
-  });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+	Meteor.startup(function () {
     // code to run on server at startup
-  });
+});
 }
