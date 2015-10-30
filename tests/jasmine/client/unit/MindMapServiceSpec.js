f75a5434fdfe54d3c1da3fbfb44817b47cca8237
amolrv <amolrv@thoughtworks.com>
2015-10-30 11:14:03 +0530

    Merged.

--- tests/jasmine/client/unit/MindMapServiceSpec.js
+++ tests/jasmine/client/unit/MindMapServiceSpec.js
@@ -6,46 +6,24 @@
 /* globals Player: false, Song: false */
 
 describe('MindMapService', function () {
-  var mindMapService;
+    var mindMapService;
 
-  beforeEach(function () {
-    mindMapService = new MindMapService();
-  });
-
-  describe("createRootNode", function () {
-    it('should return id after inserting node in DB', function () {
-
-      var mindmapId = 1, name = 'string'
-        , result = { name: name, children : [], direction : null };
-      spyOn(Mindmaps, 'insert').and.returnValue(mindmapId);
-
-      expect(mindMapService.createRootNode(name)).toBe(mindmapId);
-      expect(Mindmaps.insert.calls.argsFor(0)).toEqual([result]);
+    beforeEach(function () {
+        mindMapService = new MindMapService();
     });
-  });
 
-   describe("updateNode", function () {
-    it('state of the object should remain same after DB update', function () {
-      var node = { _id: 1, children: [],direction: "right", name: 'someNode' },
-        key = { _id: node._id },
-        updatedMap = { children: [],direction:"right", name: 'someNode' };
-      spyOn(Mindmaps, 'update');
-      mindMapService.updateNode(node);
-      expect(Mindmaps.update.calls.argsFor(0)).toEqual([key, updatedMap]);
+
+    describe("addChild", function () {
+        it('Should add a node to an existing node', function () {
+            var name = "new mindmap", dir = "right",
+                parent_node = {name: name, children: [], direction: dir},
+                new_node = {name: name, children: [], direction: dir};
+            mindMapService.addChild(parent_node);
+            parent_node.children[0] = new_node;
+            expect(parent_node.children.length).toBe(1);
+
+        });
     });
-  });
-
-  describe("addChild", function(){
-    it('Should add a node to an existing node', function(){
-      var name = "new mindmap", dir = "right"
-      parent_node = {name:name,children:[],direction:dir},
-      new_node = {name:name,children:[],direction:dir};
-      mindMapService.addChild(parent_node);
-      parent_node.children[0] = new_node;
-      expect(parent_node.children.length).toBe(1);
-
-    });
-  });
 
 });
 
