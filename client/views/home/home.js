734a91bd6fba75dd44a86db77a14130df41627c6
Rupali Gangarde <rupalig@thoughtworks.com>
2015-12-11 15:24:44 +0530

    Home page mindmap images added

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -12,3 +12,28 @@ Template.MyButton.events({
 		localStorage.clear();
 	}
 });
+
+var i = 0;
+var path = new Array();
+
+// LIST OF IMAGES
+path[0] = "SliderImage1-Schedule.png";
+path[1] = "SliderImage2-Pune.png";
+path[2] = "SliderImage3-MovieDialogues.png";
+
+function swapImage()
+{
+
+   setInterval(function() {
+	   var img = document.querySelector(".home-bg img");
+	   if(img)
+		img.src = path[i];
+	   if(i < path.length - 1) i++; else i = 0;
+   }, 2000)
+}
+swapImage();
+
+
+$(".aboutus").click(function() {
+	$('html, body').animate({scrollTop: $("#aboutus").offset().top}, "slow");
+})
