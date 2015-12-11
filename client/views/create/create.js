24afc87f2f98dfc84b5b00d0d3d4863158dc6477
amolrv <amolrv@thoughtworks.com>
2015-12-11 14:16:23 +0530

    [Amol/Jayanta] Added arrow key debouncing

--- client/views/create/create.js
+++ client/views/create/create.js
@@ -265,7 +265,7 @@ Editor.prototype.setupAttributes = function () {
                 e.preventDefault();
             }
 
-            if(e.keyCode == 9) {
+            if (e.keyCode == 9) {
                 e.stopPropagation();
                 e.preventDefault();
                 updateNode(editor, editBox, nodeData, currentTextElement);
@@ -877,261 +877,250 @@ Mousetrap.bind('mod+e', function createXmlFile() {
 
 });
 
-Mousetrap.bind('mod+left', function () {
-    // left key pressed
-    var event = arguments[0];
-    (event.preventDefault || event.stop || event.stopPropagation || function () {
-    }).call(event);
-    var selection = d3.select(".node.selected")[0][0];
-    if (selection) {
-        var data = selection.__data__;
-        var dir = getDirection(data),
-            parent = data.parent,
+Mousetrap.bind('mod+left', debounce(250, true,
+    function () {
+        var selection = d3.select(".node.selected")[0][0];
+        if (selection) {
+            var data = selection.__data__;
+            var dir = getDirection(data),
+                parent = data.parent,
+                selectedNode,
+                target,
+                direction;
+
+            function pasteAfterCut() {
+                if (target.isCollapsed) {
+                    expandRecursive(target, target._id);
+                    removeLocally(target._id);
+                }
+                if (direction == "right")
+                    selectedNode = paste(data, target, direction, parent);
+                else
+                    selectedNode = paste(data, target, direction);
+                retainCollapsed();
+                selectNode(selectedNode);
+            }
+
+            switch (dir) {
+                case('right'):
+                    cut(function () {
+                        if (getDirection(parent) === 'root') {
+                            if (data.hasOwnProperty('isCollapsed') && data.isCollapsed)
+                                removeLocally(data._id);
+                            selectedNode = paste(data, parent, "left");
+                            retainCollapsed();
+                            selectNode(selectedNode);
+                            return;
+
+                        }
+                        else {
+                            target = parent.parent;
+                            direction = "right";
+                            pasteAfterCut();
+                        }
+                    });
+
+                    break;
+                case('root'):
+                    alert("Root cannot be added to a new parent");
+                    break;
+                case('left'):
+                    var nl = parent.children || [], i = 0;
+                    if (parent[dir]) {
+                        nl = parent[dir];
+                    }
+                    var l = nl.length;
+                    for (; i < l; i++) {
+                        if (nl[i]._id === data._id && l != 1) {
+                            if (i === 0)
+                                target = nl[(i + 1)];
+                            else
+                                target = nl[(i - 1)];
+                            direction = "left";
+                            cut(pasteAfterCut);
+                            break;
+                        }
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }));
+
+Mousetrap.bind('mod+right', debounce(250, true,
+    function () {
+        var selection = d3.select(".node.selected")[0][0],
             selectedNode,
             target,
             direction;
 
-        function pasteAfterCut() {
-            if (target.isCollapsed) {
-                expandRecursive(target, target._id);
-                removeLocally(target._id);
+
+        if (selection) {
+            var data = selection.__data__;
+            var dir = getDirection(data),
+                parent = data.parent;
+
+            function pasteAfterCut() {
+                if (target.isCollapsed) {
+                    expandRecursive(target, target._id);
+                    removeLocally(target._id);
+                }
+                if (direction == "left")
+                    selectedNode = paste(data, target, direction, parent);
+                else
+                    selectedNode = paste(data, target, direction);
+                retainCollapsed();
+                selectNode(selectedNode);
             }
-            if (direction == "right")
-                selectedNode = paste(data, target, direction, parent);
-            else
-                selectedNode = paste(data, target, direction);
-            retainCollapsed();
-            selectNode(selectedNode);
-        }
 
-        switch (dir) {
-            case('right'):
-                cut(function () {
-                    if (getDirection(parent) === 'root') {
-                        if (data.hasOwnProperty('isCollapsed') && data.isCollapsed)
-                            removeLocally(data._id);
-                        selectedNode = paste(data, parent, "left");
-                        retainCollapsed();
-                        selectNode(selectedNode);
-                        return;
+            switch (dir) {
+                case('left'):
+                    cut(function () {
+                        if (getDirection(parent) === 'root') {
+                            if (data.hasOwnProperty('isCollapsed') && data.isCollapsed)
+                                removeLocally(data._id);
+                            selectedNode = paste(data, parent, "right");
+                            retainCollapsed();
+                            selectNode(selectedNode);
+                            return;
+                        }
+                        else {
+                            target = parent.parent;
+                            direction = "left";
+                            pasteAfterCut();
+                        }
+                    });
 
+                    break;
+                case('root'):
+                    alert("Root cannot be added to a new parent");
+                    break;
+                case('right'):
+                    var nl = parent.children || [], i = 0;
+                    if (parent[dir]) {
+                        nl = parent[dir];
                     }
-                    else {
-                        target = parent.parent;
-                        direction = "right";
-                        pasteAfterCut();
+                    var l = nl.length;
+                    for (; i < l; i++) {
+                        if (nl[i]._id === data._id && l != 1) {
+                            if (i === 0)
+                                target = nl[(i + 1)];
+                            else
+                                target = nl[(i - 1)];
+                            direction = "right";
+                            cut(pasteAfterCut);
+                            break;
+                        }
                     }
-                });
-
-                break;
-            case('root'):
-                alert("Root cannot be added to a new parent");
-                break;
-            case('left'):
-                var nl = parent.children || [], i = 0;
-                if (parent[dir]) {
-                    nl = parent[dir];
-                }
-                var l = nl.length;
-                for (; i < l; i++) {
-                    if (nl[i]._id === data._id && l != 1) {
-                        if (i === 0)
-                            target = nl[(i + 1)];
-                        else
-                            target = nl[(i - 1)];
-                        direction = "left";
-                        cut(pasteAfterCut);
-                        break;
-                    }
-
-                }
-                break;
-            default:
-                break;
-        }
-    }
-});
-
-Mousetrap.bind('mod+right', function () {
-    var event = arguments[0];
-    (event.preventDefault || event.stop || event.stopPropagation || function () {
-    }).call(event);
-    var selection = d3.select(".node.selected")[0][0],
-        selectedNode,
-        target,
-        direction;
-
-
-    if (selection) {
-        var data = selection.__data__;
-        var dir = getDirection(data),
-            parent = data.parent;
-
-        function pasteAfterCut() {
-            if (target.isCollapsed) {
-                expandRecursive(target, target._id);
-                removeLocally(target._id);
+                    break;
+                default:
+                    break;
             }
-            if (direction == "left")
-                selectedNode = paste(data, target, direction, parent);
-            else
-                selectedNode = paste(data, target, direction);
-            retainCollapsed();
-            selectNode(selectedNode);
+
         }
-
-        switch (dir) {
-            case('left'):
-                cut(function () {
-                    if (getDirection(parent) === 'root') {
-                        if (data.hasOwnProperty('isCollapsed') && data.isCollapsed)
-                            removeLocally(data._id);
-                        selectedNode = paste(data, parent, "right");
-                        retainCollapsed();
-                        selectNode(selectedNode);
-                        return;
-                    }
-                    else {
-                        target = parent.parent;
-                        direction = "left";
-                        pasteAfterCut();
-                    }
-                });
-
-                break;
-            case('root'):
-                alert("Root cannot be added to a new parent");
-                break;
-            case('right'):
-                var nl = parent.children || [], i = 0;
-                if (parent[dir]) {
-                    nl = parent[dir];
-                }
-                var l = nl.length;
-                for (; i < l; i++) {
-                    if (nl[i]._id === data._id && l != 1) {
-                        if (i === 0)
-                            target = nl[(i + 1)];
-                        else
-                            target = nl[(i - 1)];
-                        direction = "right";
-                        cut(pasteAfterCut);
-                        break;
-                    }
-                }
-                break;
-            default:
-                break;
-        }
-
-    }
-});
+    }));
 
 
-Mousetrap.bind('mod+up', function () {
-    var event = arguments[0];
-    (event.preventDefault || event.stop || event.stopPropagation || function () {
-    }).call(event);
-    var selection = d3.select(".node.selected")[0][0].__data__;
+Mousetrap.bind('mod+up', debounce(250, true,
+    function () {
+        var selection = d3.select(".node.selected")[0][0].__data__;
 
-    if (!(selection && selection.parent))
-        return;
-
-    var previousSibling,
-        siblings = selection.parent[selection.position] || selection.parent.children,
-        parent = selection.parent;
-    if (siblings.length <= 1) return;
-    if (selection.previous) {
-
-        if (parent[selection.position]) {
-            siblings = parent[selection.position];
-        }
-        var l = siblings.length;
-        if (l == 1)
+        if (!(selection && selection.parent))
             return;
-        for (var i = 0; i < l; i++) {
-            if (siblings[i]._id === selection._id) {
-                previousSibling = siblings[i - 1];
-                break;
+
+        var previousSibling,
+            siblings = selection.parent[selection.position] || selection.parent.children,
+            parent = selection.parent;
+        if (siblings.length <= 1) return;
+        if (selection.previous) {
+
+            if (parent[selection.position]) {
+                siblings = parent[selection.position];
             }
+            var l = siblings.length;
+            if (l == 1)
+                return;
+            for (var i = 0; i < l; i++) {
+                if (siblings[i]._id === selection._id) {
+                    previousSibling = siblings[i - 1];
+                    break;
+                }
+            }
+            if (previousSibling.previous) {
+                previousSibling = siblings.find(function (x) {
+                    return x._id == previousSibling.previous
+                });
+            }
+            else {
+                selectNode(previousSibling);
+                cut(function (err, data) {
+                    paste(previousSibling, selection.parent, selection.position, selection);
+                    retainCollapsed();
+                    selectNode(selection);
+                });
+                return;
+            }
+        } else {
+
+            previousSibling = siblings[siblings.length - 1];
         }
-        if (previousSibling.previous) {
-            previousSibling = siblings.find(function (x) {
-                return x._id == previousSibling.previous
+        cut(function (err, data) {
+            var selectedNode = paste(selection, selection.parent, selection.position, previousSibling);
+            retainCollapsed();
+            selectNode(selectedNode);
+        });
+    }));
+
+Mousetrap.bind('mod+down', debounce(250, true,
+    function () {
+        var selection = d3.select(".node.selected")[0][0].__data__;
+
+        if (!(selection && selection.parent))
+            return;
+
+        var nextSibling,
+            siblings = selection.parent[selection.position] || selection.parent.children;
+        if (siblings.length <= 1) return;
+        if (selection.next) {
+            nextSibling = siblings.find(function (x) {
+                return x._id == selection.next;
             });
+
         }
         else {
-            selectNode(previousSibling);
-            cut(function (err, data) {
-                paste(previousSibling, selection.parent, selection.position, selection);
+            var newNode = {
+                name: selection.name, position: selection.position,
+                parent_ids: selection.parent_ids,
+                previous: null, next: siblings[0]._id
+            };
+            cut(function () {
+                var headId = siblings[0]._id;
+                newNode._id = mindMapService.addNode(newNode);
+                if (selection.hasOwnProperty('isCollapsed') && selection.isCollapsed) {
+                    newNode.isCollapsed = selection.isCollapsed;
+                    storeLocally(newNode);
+                }
+
+                mindMapService.updateNode(headId, {previous: newNode._id});
+                var previous = null;
+                (selection.children || selection._children || []).forEach(function (child) {
+                    previous = paste(child, newNode, child.position, previous);
+                });
+
                 retainCollapsed();
-                selectNode(selection);
+                selectNode(newNode);
             });
             return;
         }
-    } else {
 
-        previousSibling = siblings[siblings.length - 1];
-    }
-    cut(function (err, data) {
-        var selectedNode = paste(selection, selection.parent, selection.position, previousSibling);
-        retainCollapsed();
-        selectNode(selectedNode);
-    });
-
-
-});
-
-Mousetrap.bind('mod+down', function () {
-    var event = arguments[0];
-    (event.preventDefault || event.stop || event.stopPropagation || function () {
-    }).call(event);
-    var selection = d3.select(".node.selected")[0][0].__data__;
-
-    if (!(selection && selection.parent))
-        return;
-
-    var nextSibling,
-        siblings = selection.parent[selection.position] || selection.parent.children;
-    if (siblings.length <= 1) return;
-    if (selection.next) {
-        nextSibling = siblings.find(function (x) {
-            return x._id == selection.next;
-        });
-
-    }
-    else {
-        var newNode = {
-            name: selection.name, position: selection.position,
-            parent_ids: selection.parent_ids,
-            previous: null, next: siblings[0]._id
-        };
         cut(function () {
-            var headId = siblings[0]._id;
-            newNode._id = mindMapService.addNode(newNode);
-            if (selection.hasOwnProperty('isCollapsed') && selection.isCollapsed) {
-                newNode.isCollapsed = selection.isCollapsed;
-                storeLocally(newNode);
-            }
-
-            mindMapService.updateNode(headId, {previous: newNode._id});
-            var previous = null;
-            (selection.children || selection._children || []).forEach(function (child) {
-                previous = paste(child, newNode, child.position, previous);
-            });
-
+            var selectedNode = paste(selection, selection.parent, selection.position, nextSibling);
             retainCollapsed();
-            selectNode(newNode);
+            selectNode(selectedNode);
         });
-        return;
-    }
-
-    cut(function () {
-        var selectedNode = paste(selection, selection.parent, selection.position, nextSibling);
-        retainCollapsed();
-        selectNode(selectedNode);
-    });
-});
+    }));
 
 
 function JSONtoXML(XMLString, nodeObject) {
