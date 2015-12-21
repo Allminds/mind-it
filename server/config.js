5a35cc47fe60e22244746d9c91b7c72ae4854b66
Poorva Gokhale <poorvagokhale@INPoorvag.local>
2015-12-21 11:29:39 +0530

    [Chinmay/Poorva] Change google  authetication credentials.

--- server/config.js
+++ server/config.js
@@ -3,6 +3,6 @@ ServiceConfiguration.configurations.remove({
 });
 ServiceConfiguration.configurations.insert({
     service: "google",
-    clientId: "307472912599-ei0a3ek0a62q38cuq49i7oqoe1pav3q4.apps.googleusercontent.com",
-    secret: "izgAkEEyIJqGkI6mvC7pAjKB"
+    clientId: process.env.clientId,
+    secret: process.env.secret
 });
