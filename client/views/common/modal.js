db925ce0bef01fa07da0e3c763cd29fe0936fdc4
Abhishek Khare <akhare@INakhare.local>
2015-12-24 17:04:57 +0530

    HTML refractoring: create.html, home.html. Files added common/header.html common/modal.html common/header.js common/modal.js

---
+++ client/views/common/modal.js
@@ -0,0 +1,17 @@
+Template.ModalHeader.helpers({
+    modalTagID:function(id) {
+        return id;
+    }
+});
+
+Template.ModalBody.helpers({
+    modalBodyImageSrc: function(path) {
+        return path;
+    }
+});
+
+Template.ModalPopUp.helpers({
+    modalTagID: function(id) {
+        return id;
+    }
+});
