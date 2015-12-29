0ca1b4895efce2fab5a698a026f5eac09fe1d24b
Abhishek Khare <akhare@INakhare.local>
2015-12-29 18:34:32 +0530

    Added FreeMind Import Functionality

---
+++ client/views/create/helpers/importParser.js
@@ -0,0 +1,110 @@
+App.ImportParser = (function () {
+  var instance,
+
+  populateMindMapFromXML = function(xmlNodes, parentJSONNode, mindmapService){
+    var previousSibling = null;
+    for(i = 0; i < xmlNodes.length ;i++) {
+        var nodeName = xmlNodes[i].getAttribute("TEXT") ? xmlNodes[i].getAttribute("TEXT") : "";
+        var direction = xmlNodes[i].getAttribute("POSITION") ? xmlNodes[i].getAttribute("POSITION") : parentJSONNode["position"];
+        var newNode = createNode(parentJSONNode, nodeName, direction, previousSibling);
+        newNode._id = mindmapService.addNode(newNode);
+
+        if (previousSibling && previousSibling._id) {
+            mindmapService.updateNode(previousSibling._id, {next: newNode._id});
+        }
+
+        previousSibling = newNode;
+        var childNodes = xmlNodes[i].childNodes;
+
+        if(childNodes.length > 0) {
+            populateMindMapFromXML(childNodes, newNode, mindmapService);
+        }
+
+
+    }
+  },
+
+  createNode = function (parent, newNodeName, dir, previousSibling) {
+    var newNode = {
+        name: newNodeName, position: dir,
+        parent_ids: [].concat(parent.parent_ids || []).concat([parent._id]),
+        previous: (previousSibling ? previousSibling._id : null), next: null
+    };
+
+    return newNode;
+  },
+
+  createRootNode = function(rootId, rootName) {
+    var rootNode = {
+        name: rootName, position: null,
+        children: [], _id: rootId
+    };
+
+    return rootNode;
+  }
+
+  var init = function () {
+    return {
+       createMindmapFromXML : function(XMLdom, mindmapService) {
+          this.errorMessage = "";
+          var parser = new DOMParser();
+          var xmlDoc = parser.parseFromString(XMLdom,"text/xml");
+          var documentNode = xmlDoc.documentElement;
+          var rootNode = documentNode.childNodes;
+
+          if(rootNode.length > 0 && rootNode[0].nodeName == "parsererror") {
+            this.errorMessage = "Invalid XML format";
+            return null;
+          } else if(documentNode.nodeName != "map") {
+            this.errorMessage = "Not a mindmap: Invalid mindmap file";
+            return null;
+          } else if( rootNode.length <= 0) {
+            this.errorMessage = "Not a mindmap: Root node not found";
+            return null;
+          }else if( rootNode.length > 1) {
+            this.errorMessage = "Not a mindmap: Multiple root nodes found";
+            return null;
+          }
+
+          rootNode = rootNode[0];
+          var rootChildNodes = rootNode.childNodes;
+
+          for (i = 0; i < rootChildNodes.length ;i++) {
+            if(!rootChildNodes[i].getAttribute("POSITION")) {
+                this.errorMessage = "Not a mindmap: POSITION attribute not found in child node of root node";
+                return null;
+            }
+          }
+
+          var rootNodeText = (rootNode.getAttribute("TEXT") === undefined) ? "tempText" : rootNode.getAttribute("TEXT");
+          var mindMapId = "";
+
+          if(mindmapService) {
+            mindMapId = mindmapService.createRootNode(rootNodeText);
+
+            var rootNodeJSON = createRootNode(mindMapId, rootNodeText);
+            populateMindMapFromXML(rootChildNodes, rootNodeJSON, mindmapService);
+          }
+          return mindMapId;
+      },
+
+      errorMessage: ""
+    }
+  };
+
+  var createInstance = function () {
+    var object = new init();
+    return object;
+  };
+
+  return {
+    getInstance: function () {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
+  };
+
+})();
+
