e0cf5efd85d355765df70731b1870ca9cc6557f9
Chinmay Kulkarni <chinmayk@thoughtworks.com>
2015-12-30 18:05:05 +0530

    [Chinmay/Poorva] Write tests for map.js

--- client/views/create/helpers/map.js
+++ client/views/create/helpers/map.js
@@ -1,8 +1,10 @@
 App.map = {};
-App.map.selectedNodeData = function () {
-  var selectedNode = d3.select(".node.selected")[0][0];
+
+App.map.getDataOfNodeWithClassNamesString = function (classNames) {
+  var selectedNode = d3.select(classNames)[0][0];
   return selectedNode ? selectedNode.__data__ : null;
 };
+
 App.map.addNodeToUI = function (parent, newNode) {
   var children = parent[newNode.position] || parent.children || parent._children;
   if (!children) {
@@ -22,7 +24,6 @@ App.map.addNodeToUI = function (parent, newNode) {
 };
 
 App.map.addNewNode = function (parent, newNodeName, dir, previousSibling) {
-
   if (!previousSibling) {
     var children = parent.position ? parent.children : parent[dir];
 
@@ -36,14 +37,10 @@ App.map.addNewNode = function (parent, newNodeName, dir, previousSibling) {
     previous: previousSibling._id, next: previousSibling.next
   };
   newNode._id = mindMapService.addNode(newNode);
-
   if (previousSibling._id) {
     mindMapService.updateNode(previousSibling._id, {next: newNode._id});
     mindMapService.updateNode(newNode.next, {previous: newNode._id});
   }
-
-  // let the subscribers to update their mind map :)
-
   return newNode;
 };
 
@@ -56,7 +53,6 @@ App.map.makeEditable = function (nodeId) {
 };
 
 App.map.findOne = function (node, fun) {
-
   if (fun(node)) return node;
   var children = (node.children || node._children || []),
     res = children.reduce(function (result, child) {
@@ -64,14 +60,16 @@ App.map.findOne = function (node, fun) {
     }, null);
   return res;
 };
-App.map.getNodeData = function (nodeId) {
-  var rootNode = d3.selectAll('#mindmap svg .node.level-0')[0][0];
-  var rootNodeData = rootNode ? rootNode.__data__ : null;
+
+App.map.getNodeDataWithNodeId = function (nodeId) {
+  var rootNodeData = App.map.getDataOfNodeWithClassNamesString('#mindmap svg .node.level-0');
   if (rootNodeData) {
-    return App.map.findOne(rootNodeData, function (x) {
+    var nodeData = App.map.findOne(rootNodeData, function (x) {
       return x._id == nodeId
     });
+    return nodeData;
   }
+  return null;
 };
 
 clone = function (node) {
