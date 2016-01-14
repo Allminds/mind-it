1d95048fac440de0555fcafff26f1eb2cb41d200
Pratima Chainani <pratimac@thoughtworks.com>
2016-01-14 14:55:12 +0530

    [Justin/Swapnil/Pratima] Refactoring

--- client/views/create/helpers/helper.js
+++ client/views/create/helpers/helper.js
@@ -222,6 +222,37 @@ App.calculateDirection = function (parent) {
   return dir;
 };
 
+App.storeLocally = function (node) {
+  var state = {isCollapsed: node.isCollapsed};
+  store.set(node._id, state);
+};
+
+App.removeLocally = function (node) {
+  store.remove(node._id);
+};
+
+App.isLocallyCollapsed = function (id) {
+  try {
+    var locallyCollapsed = store.get(id).isCollapsed;
+  }
+  catch (e) {
+  }
+  return locallyCollapsed ? true : false;
+};
+
+App.retainCollapsed = function  () {
+  store.forEach(function (key) {
+    try {
+      if (App.isLocallyCollapsed(key)) {
+        var nodeData = App.map.getNodeDataWithNodeId(key);
+        App.collapse(nodeData, key);
+      }
+    }
+    catch (e) {
+    }
+  });
+};
+
 var collapseRecursive = function (d, id) {
   if (d._id === id) {
     d.isCollapsed = true;
