faf03f1df2e0a1f5e7326f2df2ee6fe63b78fdf5
Justin Jose <justinj@thoughtworks.com>
2016-01-18 14:39:44 +0530

    [Jayantha/Justin] Fix collaboration issues relating to drag drop, collapse expand, reposition

--- client/views/create/helpers/tracker.js
+++ client/views/create/helpers/tracker.js
@@ -5,15 +5,16 @@ App.tracker = {
       return;
     newNode = fields;
     newNode._id = id;
+    
     var parent = App.map.getNodeDataWithNodeId(newNode.parentId);
+    App.nodeStore[id] = newNode;
     App.map.addNodeToUI(parent, newNode);
     App.nodeSelector.setPrevDepth(newNode.depth);
   },
   changed: function (id, fields) {
     var updatedNode = App.map.getNodeDataWithNodeId(id);
     if (!updatedNode) return;
-    
-    if (fields.hasOwnProperty('name')) {
+     if (fields.hasOwnProperty('name')) {
       updatedNode.name = fields.name;
       App.chart.update();
       var selectedNode = App.map.getDataOfNodeWithClassNamesString(".node.selected");
@@ -25,15 +26,17 @@ App.tracker = {
       }
     } else if (fields.hasOwnProperty('childSubTree') || fields.hasOwnProperty('left') || fields.hasOwnProperty('right')){
       var parent = App.map.getNodeDataWithNodeId(id),
-          key = Object.keys(fields)[0],
-          subTree = App.Node.isRoot(parent) ? parent[key] : (parent.isCollapsed ? parent._childSubTree : parent.childSubTree),
+          isCollapsed = parent.isCollapsed;
+      var key = Object.keys(fields)[0],
+          subTree = App.Node.getSubTree(parent, key),
           childIds = fields[key],
+          selectedNode = App.map.getDataOfNodeWithClassNamesString(".node.selected"),
           newlyAddedId = App.getNewlyAddedNodeId(parent, fields),
           newSubTree = childIds.map(
             function(id){
               return subTree.find(
                 function(node){
-                  return node._id == id;
+                  return node._id === id;
                 });
             });
       
@@ -44,31 +47,43 @@ App.tracker = {
       }
           
       if(App.tracker.updatedNodeId == null){
-        parent[key] = newSubTree;
+        App.Node.setSubTree(parent, newSubTree, key);
         App.chart.update();
+        if(App.tracker.repaintNodeId) {
+          var node = d3.selectAll(".node")[0].find(
+            function(child){
+              return child.__data__._id == App.tracker.repaintNodeId;
+            });
+          if(node) {
+            var tempD3Array = d3.select('thisIsANonExistentTag');
+            tempD3Array[0].pop();
+            tempD3Array[0].push(node);
+            App.removeAllLevelClass(tempD3Array);
+            App.applyLevelClass(tempD3Array, node.__data__.depth);
+            App.applyClassToSubTree(node.__data__, null, App.removeAllLevelClass);
+            App.applyClassToSubTree(node.__data__, null, App.applyLevelClass, parentDepth+2);
+          }
+          App.tracker.repaintNodeId = null;
+        }
+        App.chart.update(); 
       } else {
         var movedNode = App.map.getNodeDataWithNodeId(App.tracker.updatedNodeId);
-        var newParentNode = App.map.getNodeDataWithNodeId(id);
-        newParentNode[key].splice(childIds.indexOf(App.tracker.updatedNodeId),0,movedNode);
+        subTree.splice(childIds.indexOf(App.tracker.updatedNodeId),0,movedNode);
         App.tracker.updatedNodeId = null;
       }
     }
     else if(fields.hasOwnProperty('parentId')) {
       App.tracker.updatedNodeId = id;
-      
+      App.tracker.repaintNodeId = id;
       var parentDepth = App.map.getNodeDataWithNodeId(fields.parentId).depth,
           selectedNode = App.map.getNodeDataWithNodeId(id),
           newParent = App.map.getNodeDataWithNodeId(fields.parentId);
       selectedNode.parent = newParent;
-      selectedNode.parentId = newParent._id;
-      var node = d3.selectAll(".node")[0].find(
-        function(child){
-          return child.__data__._id == id;
-        });
-      node.setAttribute('class', "node level-"+(parentDepth+1) +" selected");
-      App.applyClassToSubTree(node.__data__, null, App.removeAllLevelClass);
-      App.applyClassToSubTree(node.__data__, null, App.applyLevelClass, parentDepth+2);
+      selectedNode.parentId = newParent._id; 
     }
+    
+    
   },
-  updatedNodeId: null
+  updatedNodeId: null,
+  repaintNodeId: null
 };
