cf29bccf2f92d59713e46b204ab904f333142877
Omkar Patil <omkar.9194@gmail.com>
2016-01-22 20:41:37 +0530

    [Omkar/Souvik:- Migration getting laid. putting on staging wwith filter and pushing migrated data file from localhost]

--- server/startup/default.js
+++ server/startup/default.js
@@ -1,6 +1,7 @@
 mindMapService = App.MindMapService.getInstance();
 Meteor.publish('mindmap', function (id) {
-  return Mindmaps.find({});
+ return Mindmaps.find({$or:[{_id:id},{rootId:id}]});
+//    return Mindmaps.find({});
 });
 Meteor.publish('userdata', function () {
   return Meteor.users.find(this.userId);
