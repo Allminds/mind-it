a4f7579d043c4772b4c3db0583e3b42e3dabf577
KJAYANTA <t3pleni9>
2016-01-27 14:54:53 +0530

    [Jayanta] Parsing spl characters for xml creation.

--- client/views/create/helpers/exportParser.js
+++ client/views/create/helpers/exportParser.js
@@ -8,45 +8,35 @@ App.exportParser.export = function (rootNodeName) {
 
 var XMLString = [];
 App.JSONConverter = function () {
-
     XMLString = "<map version=\"1.0.1\">\n";
     var rootNode = Mindmaps.find({position: null , rootId: null}).fetch()[0];
-
-    XMLString += "<node ID=\"" + rootNode._id + "\" TEXT=\"" + rootNode.name + "\" >\n";
-
+    XMLString += "<node ID=\"" + rootNode._id + "\" TEXT=\"" + parseSymbols(rootNode.name) + "\" >\n";
     var leftChildren = rootNode.left;
     var rightChildren = rootNode.right;
 
     leftChildren.forEach(function (nodePassed) {
-
         var id = "" + nodePassed;
         var name = Mindmaps.find({_id: id}).fetch()[0].name;
-
-        XMLString += "<node ID=\"" + id + "\" TEXT=\"" + name + "\" POSITION=\"left\"" + " >\n";
-
+        XMLString += "<node ID=\"" + id + "\" TEXT=\"" + parseSymbols(name) + "\" POSITION=\"left\"" + " >\n";
         children_recurse(id);
-
         XMLString += "</node>\n";
-
     });
     rightChildren.forEach(function (nodePassed) {
-
         var id = "" + nodePassed;
         var name = Mindmaps.find({_id: id}).fetch()[0].name;
-
-        XMLString += "<node ID=\"" + id + "\" TEXT=\"" + name + "\" POSITION=\"right\"" + " >\n";
-
+        XMLString += "<node ID=\"" + id + "\" TEXT=\"" + parseSymbols(name) + "\" POSITION=\"right\"" + " >\n";
         children_recurse(id);
-
         XMLString += "</node>\n";
     });
 
     XMLString += "</node>\n";
     XMLString += "</map>";
-    //console.log(XMLString);
     return XMLString;
 };
 
+var parseSymbols = function(name){
+    return name.replace('&', '&amp;').replace('"', "&quot;").replace("'", '&apos;').replace('<', '&lt;').replace('>', '&gt;');
+};
 
 var children_recurse = function (id) {
 
