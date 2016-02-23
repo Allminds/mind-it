854aa2c2603062720e4c98f0e92f4556dc1b4b65
anubha saxena <anubhasaxena@INanubhas.local>
2016-02-23 10:56:54 +0530

    Responsive UI changes

--- client/views/common/header.js
+++ client/views/common/header.js
@@ -1,5 +1,30 @@
+Template.TopBar.events({
+  'click [data-action=login]': function (e, args) {
+    e.preventDefault();
+    Meteor.loginWithGoogle();
+  },
+
+  'click [data-action=logout]': function (e, args) {
+    e.preventDefault();
+    Meteor.logout();
+  },
+  'click [data-action=toggleOptions]': function (e, args) {
+    e.preventDefault();
+    $("div#userOptions").toggle();
+  }
+});
+
 Template.TopBar.helpers({
   topbarTagID: function (id) {
     return id;
+  },
+
+  name: function () {
+    return Meteor.user().services.google.given_name;
+  },
+
+  picture: function(){
+    if(Meteor.user())
+      return Meteor.user().services.google.picture;
   }
 });
