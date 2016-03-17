a611318ef09ac235333db554ee5767acc9efdbd0
SwapnilBGaikwad <swapnilbgaikwad99@gmail.com>
2016-03-17 20:48:05 +0530

    [Swapnil] Revert chenges in config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ ServiceConfiguration.configurations.remove({
   service: "google"
 });
 ServiceConfiguration.configurations.insert({
-  service: process.env.service ,
-  clientId: process.env.clientId,
-  secret: process.env.secret
+   service: "google",
+   clientId: "898854004180-l4en162tc2gmau04natb48n0oobmqim0.apps.googleusercontent.com",
+   secret: "TLsWhYSFBI5_nIVCEiHmhjXk"
 });
