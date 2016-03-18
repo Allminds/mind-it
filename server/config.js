285f1e25b63deb8687bd99b55f4ec10fb41e6fe2
Dhananjay Khaparkhuntikar <dhananjay@INdhanank.local>
2016-03-18 11:43:08 +0530

    [Dhananjay/Swapnil] Removing publically available creadentials in config.js file

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ ServiceConfiguration.configurations.remove({
   service: "google"
 });
 ServiceConfiguration.configurations.insert({
-   service: "google",
-   clientId: "898854004180-l4en162tc2gmau04natb48n0oobmqim0.apps.googleusercontent.com",
-   secret: "TLsWhYSFBI5_nIVCEiHmhjXk"
+   service: process.env.service,
+   clientId: process.env.clientId,
+   secret: process.env.secret
 });
