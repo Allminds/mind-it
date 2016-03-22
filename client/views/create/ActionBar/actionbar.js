212f3b28881a39d2b9d2b51d242318e83f0bdc71
Omkar Patil <omkar.9194@gmail.com>
2016-03-22 13:19:14 +0530

    [Dhanajay/Omkar] Showing online users on the action bar with profile pictures.

--- client/views/create/ActionBar/actionbar.js
+++ client/views/create/ActionBar/actionbar.js
@@ -1,8 +1,16 @@
 Template.ActionBar.helpers({
     userimages: function(){
         var usersAvailable= Meteor.users.find().fetch();
-        var imageSrcs= users.map(function(){
-            return usersAvailable.services.google.picture;
+        var Srcs= usersAvailable.map(function(x){
+        if(x.status.online )
+          {
+        //console.log("status",x.status.online);
+            return x.services.google.picture;
+          }
+        });
+        var imageSrcs=Srcs.filter(function(y)
+        {
+            return typeof y === 'string';s
         });
         console.log("IMAGES: ",imageSrcs);
         return imageSrcs;
