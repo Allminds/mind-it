b04b2f3961754447fc564d12d37e73a8d08748f1
Omkar Patil <omkar.9194@gmail.com>
2016-03-22 18:04:06 +0530

    [Omkar/Dhananjay] Added profile names as well as profile pictures to the action bar of mindmap page.

--- client/views/create/ActionBar/actionbar.js
+++ client/views/create/ActionBar/actionbar.js
@@ -1,17 +1,28 @@
 Template.ActionBar.helpers({
+//var json_array = [ ];
     userimages: function(){
         var usersAvailable= Meteor.users.find().fetch();
         var Srcs= usersAvailable.map(function(x){
-        if(x.status.online )
+                var user_object;
+
+        if(x.status.online && App.currentMap == x.mindmap_id)
           {
         //console.log("status",x.status.online);
-            return x.services.google.picture;
+//        user_object.name=x.services.google.name;
+//        user_object.picture=x.services.google.picture;
+            return { name: x.services.google.name, picture: x.services.google.picture};
           }
         });
+        console.log("before filter: ", Srcs);
         var imageSrcs=Srcs.filter(function(y)
         {
-            return typeof y === 'string';s
+            if (y === undefined || y === null) {
+                 // do something
+            }
+            else
+            return y;
         });
+        console.log("RETURNED: ", imageSrcs);
         return imageSrcs;
     }
 });
