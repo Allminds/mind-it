6b7620bc11137c8460da1498844e2fdeb3db9542
Dhananjay Khaparkhuntikar <dhananjay@INdhanank.local>
2016-03-23 14:52:03 +0530

    [Dhananjay/Omkar]Edit navigation bar for create page

--- client/views/layouts/Export.js
+++ client/views/layouts/Export.js
@@ -7,6 +7,16 @@ Template.Export.events({
         var rootName = d3.select(".node.level-0")[0][0].__data__.name;
         App.exportParser.export(rootName);
         //App.exportParser.export();
-    }
+    },
 
+
+});
+Template.ExportImageButton.events({
+    'click': function (e ,args) {
+        e.preventDefault();
+        console.log("in here");
+        var rootName = d3.select(".node.level-0")[0][0].__data__.name;
+        App.exportParser.export(rootName);
+        //App.exportParser.export();
+    }
 });
