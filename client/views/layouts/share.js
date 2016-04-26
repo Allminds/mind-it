d25486078a346eb56baba4afce5b954e84ccaba4
Dhananjay Khaparkhuntikar <dhananjay@INdhanank.local>
2016-04-26 14:44:29 +0530

    [Dhananjay]Remove unnecessary action binding in JS files for templates

--- client/views/layouts/share.js
+++ client/views/layouts/share.js
@@ -33,38 +33,3 @@ Template.ShareImageButton.helpers({
     }
 
 });
-
-Template.Share.events({
-    'click #shareImageButton': function () {
-        $('#share-modal').modal('show');
-
-    },
-    'click #getSharableWriteLink': function () {
-
-        Meteor.call("getSharableWriteLink", App.currentMap, function (error, value) {
-            var msg = value;
-        });
-    },
-    'click #getSharableReadLink': function () {
-        Meteor.call("getSharableReadLink", App.currentMap, function (error, value) {
-            var msg = value;
-        });
-    }
-});
-Template.Share.helpers({
-    shareReadLink: function () {
-        if (App.isSharedMindmap == App.Constants.Mode.READ) {
-            return "";
-        }
-        else
-            return "Share read only link";
-    },
-    shareWriteLink: function () {
-        if (App.isSharedMindmap == App.Constants.Mode.WRITE) {
-            return "";
-        }
-        else
-            return "Share read-write link";
-    }
-
-});
